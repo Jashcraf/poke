8a8671d81d6a4cdf0df60d1dd744f674774a9149
Jashcraf <jashcraft@email.arizona.edu>
2022-07-13 14:51:14 -1000

    raytracing for multiple surfaces

--- test_rayclass.py
+++ test_rayclass.py
@@ -15,7 +15,7 @@ pth = "C:/Users/jaren/Desktop/poke/Hubble_Test.zmx"
 # Initialize a ray bundle
 raybundle = ray.RayBundle(nrays,n1,n2)
 # try to run a raytrace
-raybundle.TraceThroughZOS(pth,[2])
+raybundle.TraceThroughZOS(pth,[2,4,7])
 raybundle.ConvertRayDataToPRTData()
 raybundle.ComputePRTMatrix()
 
@@ -28,7 +28,9 @@ raybundle.ComputePRTMatrix()
 # ZOS Single Raytrace says the edge of the pupil has an AOI of ~6.2 degrees, does that track?
 # plt.AOIPlot(raybundle)
 # plt.JonesPlot(raybundle)
-plt.PRTPlot(raybundle)
+plt.PRTPlot(raybundle,surf=0)
+plt.PRTPlot(raybundle,surf=1)
+plt.PRTPlot(raybundle,surf=2)
 # plt.PlotRays(raybundle)
 
 
