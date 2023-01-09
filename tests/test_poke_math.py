5dbe9d4527af76b3f433d7bc7a2962a67c1ab755
Jashcraf <jashcraft@arizona.edu>
2023-01-08 17:59:42 -0700

    test commit

--- tests/test_poke_math.py
+++ tests/test_poke_math.py
@@ -3,6 +3,9 @@ import poke.poke_math as mth
 
 def test_rotation3D():
 
+    """ want a matrix we can spin, consult wikipedia page
+    """
+
     angle = .1
     axis = np.array([0.,1.,0.],dtype='float64')
     
