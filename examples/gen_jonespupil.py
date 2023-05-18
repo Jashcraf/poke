b938ccae1739aee94618174fb353898f1db65a94
Jashcraf <jashcraft@arizona.edu>
2023-05-18 07:28:33 -0700

    Revert "new gen_jonespupil.py for zos"
    This reverts commit b8600041fe692a21303cd782cdc8f881a06b7dc3.

--- examples/gen_jonespupil.py
+++ examples/gen_jonespupil.py
@@ -1,6 +1,5 @@
-import numpy as np
 import poke.poke_core as pol
-import poke.plotting as plot
+import poke.raytrace as ray
 
 # Initialize a Raybundle
 nrays = 128
@@ -14,36 +13,16 @@ pth = "C:/Users/douglase/Desktop/poke/test_files/Hubble_Test.zmx"
 # Surface parameters are defined with Python dictionaries.
 # 'surf' is the surface number in the raytrace lens data editor
 # 'coating' is the thin film information. For a single layer, it's just the refractive index
-
-# Primary
 s1 = {
     'surf':2,
-    'coating':n2,
-    'mode':'reflect'
+    'coating':n2
 }
 
-# Secondary
 s2 = {
     'surf':4,
-    'coating':n2,
-    'mode':'reflect'
+    'coating':n2
 }
 
-# Fold Mirror
-s3 = {
-    'surf':8,
-    'coating':n2,
-    'mode':'reflect'
-}
-
-# Initialize Rayfront
 raybundle = pol.Rayfront(nrays,wlen,radius,ffov,circle=True)
-raybundle.as_polarized([s1,s2,s3]) # pass the raybundle the surface list
-raybundle.trace_rayset(pth)
-
-# Jones pupil computation
-raybundle.compute_jones_pupil(aloc=np.array([0.,-1.,0.]))
-
-# Plot it
-plot.jones_pupil(raybundle)
-
+raybundle.as_polarized([s1,s2]) # pass the raybundle the surface list
+raybundle.TraceRaysetZOS(pth,surfaces=[s1,s2])
