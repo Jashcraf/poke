38e144c97a630d64f369e836bf261365fe5f33ce
Jaren Ashcraft <jashcraft@email.arizona.edu>
2023-09-25 10:43:57 -0700

    re-running autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('poke')
+sys.path.insert(0, os.path.abspath('poke'))
 
 
 # -- Project information -----------------------------------------------------
